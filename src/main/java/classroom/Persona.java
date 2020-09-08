c2c118d1b031d169a928cde77f0563e0ac312772
AMILDER07 <amilder55@gmail.com>
2020-09-08 17:51:46 -0500

    Test 1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,13 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    private long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        // cedula = 3;
     }
 
     Persona(long cedula, String nombre) {
--- src/main/java/classroom/Taller2.java
+++ src/main/java/classroom/Taller2.java
@@ -5,7 +5,7 @@ public class Taller2 {
     public static void main(String[] args) {
         Persona[] estudiantes1 = new Persona[3];
         estudiantes1[0] = new Persona(188220);
-        estudiantes1[1] = new Persona();
+        estudiantes1[1] = new Persona(0);
         estudiantes1[2] = new Persona(71212, "Maria");
         Persona profesor1 = new Persona("Juan", 988);
         Asignatura asignatura1 = new Asignatura("Calculo");
@@ -25,7 +25,7 @@ public class Taller2 {
         System.out.println(asignatura2.codigoExterno);
         asignatura1.cambiarDatos(50);
         System.out.println(asignatura1.codigoExterno);
-        System.out.println(estudiantes1[1].cedula);
-        System.out.println(new Persona("Alejandro").cedula);
+        System.out.println(estudiantes1[1].getCedula());
+        System.out.println(new Persona("Alejandro", 1).getCedula());
     }
 }
